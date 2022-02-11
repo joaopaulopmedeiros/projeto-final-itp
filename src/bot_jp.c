d61ce840b349bc0cfbea8a5addbd676e04d981f7
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-02-11 14:03:30 -0300

    refactor: player typedef

--- src/bot_jp.c
+++ src/bot_jp.c
@@ -16,10 +16,15 @@ typedef struct {
   Point** points; //pontos de que o mapa é composto 
 } Map;
 
-// lê os dados do jogo e atualiza os dados do bot
-void readData(char* myId, Map* map) {
+typedef struct {
+  int totalBotsPlaying;
   char id[MAX_LINE];
-  int v, n, x, y;
+} Player;
+
+// lê os dados do jogo e atualiza os dados do bot
+void readData(Player* player, Map* map) {
+  char id[MAX_LINE];
+  int v, x, y;
 
   for (int i = 0; i < map->height; i++) {   
     for (int j = 0; j < map->width; j++) {
@@ -28,8 +33,9 @@ void readData(char* myId, Map* map) {
   }
 
   //lê a quantidade de bots
-  scanf(" BOTS %i", &n); 
-  for (int i = 0; i < n; i++) {
+  scanf(" BOTS %i", &player->totalBotsPlaying); 
+
+  for (int i = 0; i < player->totalBotsPlaying; i++) {
     // lê o id dos bots e suas posições
     scanf("%s %i %i", id, &x, &y); 
   }
@@ -42,20 +48,19 @@ char* chooseCommand(Map* map)
 
 int main() {
   char line[MAX_LINE];
-  char myId[MAX_LINE];
+  Map map;
+  Player player;
 
   setbuf(stdin, NULL);
   setbuf(stdout, NULL); 
   setbuf(stderr, NULL);
 
-  Map map;
-
   scanf("AREA %i %i", &map.height, &map.width);
-  scanf(" ID %s", myId);    
-  fprintf(stderr, "%s\n", myId);
+  scanf(" ID %s", player.id);    
+  fprintf(stderr, "%s\n", player.id);
 
   while (1) {
-    readData(myId, &map);
+    readData(&player, &map);
     char* command = chooseCommand(&map);
     printf("%s\n", command);
     scanf("%s", line);
