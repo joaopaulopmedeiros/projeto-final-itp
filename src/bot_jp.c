3eb27a1e427ea0daf57458f5b35044676db7a9bb
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-02-11 16:15:01 -0300

    feat: (wip) make decision

--- src/bot_jp.c
+++ src/bot_jp.c
@@ -33,7 +33,7 @@ typedef struct {
   Stock stock; //estoque de peixes
 } Player;
 
-// informa se ponto é apenas mar aberto
+//informa se ponto é apenas mar aberto
 bool isEmptySeaArea(int value) {
   if(value == 0) {
     return true;
@@ -42,7 +42,7 @@ bool isEmptySeaArea(int value) {
   }
 }
 
-// informa se ponto é um porto
+//informa se ponto é um porto
 bool isHarborArea(int value) {
   if(value == 1) {
     return true;
@@ -51,9 +51,39 @@ bool isHarborArea(int value) {
   }
 }
 
+//informa se é região proibida para pesca (há zero quilos de qualquer um dos três tipos de peixe)
+bool isForbbidenFishingArea(int value) {
+  if(value == 10 || value == 20 || value == 30) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //escolhe comando do bot (movimentação, pesca ou venda) conforme situação do mapa
 char* chooseCommand(Player* player, Map* map) {
-  return "LEFT";
+  int distance = 1; //distância entre ponto e meu usuário - neste caso, varre uma casa
+
+  //valor encontrado na posição atual do bot
+  int value = map->points[player->x][player->y].value;
+
+  char* command = malloc(sizeof(char) * 10);
+
+  command = "LEFT";
+
+  if(isEmptySeaArea(value)) {
+    //command = move();
+  } else if(isHarborArea(value)) {
+    //command = sell();
+  } else {
+    if(!isForbbidenFishingArea(value)) {
+      //command = fish();
+    } else {
+      //command = move();
+    }
+  }
+
+  return command;
 }
 
 // lê os dados do jogo e atualiza o mapa conforme posições dos bots adversários
